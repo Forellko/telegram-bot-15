b36164a2b837ec359cd9f75496d54b3b312bd13f
Forellko <dragonforce97q@gmail.com>
2022-12-08 19:54:18 +0300

    Add option.js for helpers

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { gameOptionsl, againOptions } = require('./options');
+
 const TelegramAPI = require('node-telegram-bot-api');
 
 const token = '5515532924:AAFeu3va7IjgPAECs4nUOAzblw1MLqDZBJs';
@@ -5,35 +7,6 @@ const token = '5515532924:AAFeu3va7IjgPAECs4nUOAzblw1MLqDZBJs';
 const bot = new TelegramAPI(token, { polling: true });
 
 const chats = {};
-
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [
-        { text: '1', callback_data: '1' },
-        { text: '2', callback_data: '2' },
-        { text: '3', callback_data: '3' },
-      ],
-      [
-        { text: '4', callback_data: '4' },
-        { text: '5', callback_data: '5' },
-        { text: '6', callback_data: '6' },
-      ],
-      [
-        { text: '7', callback_data: '7' },
-        { text: '8', callback_data: '8' },
-        { text: '9', callback_data: '9' },
-      ],
-      [{ text: '0', callback_data: '0' }],
-    ],
-  }),
-};
-
-const againOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [[{ text: 'Играть еще раз', callback_data: '/again' }]],
-  }),
-};
 
 const startGame = async (chatID) => {
   const randomNumber = Math.floor(Math.random() * 10);
