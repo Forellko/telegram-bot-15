ebb95de1e00cda7064ba24a0de6f507bfd941fd6
Forellko <dragonforce97q@gmail.com>
2022-12-08 18:38:56 +0300

    first response from bot

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const TelegramAPI = require('node-telegram-bot-api');
+
 const token = '5515532924:AAFeu3va7IjgPAECs4nUOAzblw1MLqDZBJs';
 
+const bot = new TelegramAPI(token, { polling: true });
 
+bot.on('message', (msg) => {
+  const text = msg.text;
+  const chatID = msg.chat.id;
+
+  bot.sendMessage(chatID, `Ты написал: ${text}`);
+});
