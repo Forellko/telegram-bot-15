c4d9094633db8e0d012a7545ac2f3076cef0e3a5
Forellko <dragonforce97q@gmail.com>
2022-12-08 18:49:35 +0300

    setMyComands

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ const token = '5515532924:AAFeu3va7IjgPAECs4nUOAzblw1MLqDZBJs';
 
 const bot = new TelegramAPI(token, { polling: true });
 
-bot.on('message', (msg) => {
+bot.setMyCommands([
+  {
+    command: '/start',
+    description: 'Приветствие',
+  },
+  { command: '/info', description: 'Получить информацию' },
+]);
+
+bot.on('message', async (msg) => {
   const text = msg.text;
   const chatID = msg.chat.id;
 
   if (text === '/start') {
-    bot.sendSticker(
+    await bot.sendSticker(
       chatID,
       'https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/5.webp'
     );
-    bot.sendMessage(chatID, `Welcome ${msg.from.username}`);
+    await bot.sendMessage(chatID, `Welcome ${msg.from.username}`);
   }
 
   if (text === '/info') {
-    bot.sendMessage(
+    await bot.sendMessage(
       chatID,
       `Тебя зовут: ${msg.from.first_name} ${msg.from.last_name}`
     );
