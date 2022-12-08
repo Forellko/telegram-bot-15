1dbbd5de25dcd44425c8c0cc2191465a00718345
Forellko <dragonforce97q@gmail.com>
2022-12-08 18:46:52 +0300

    Send sticker :)

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ bot.on('message', (msg) => {
   const chatID = msg.chat.id;
 
   if (text === '/start') {
+    bot.sendSticker(
+      chatID,
+      'https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/5.webp'
+    );
     bot.sendMessage(chatID, `Welcome ${msg.from.username}`);
   }
 
